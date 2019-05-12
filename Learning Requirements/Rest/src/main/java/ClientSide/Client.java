7fd1ed7c305d394e931a2d5ea1ee92a8f28ee793
amirali monjar <amirali.mnj@gmail.com>
2019-05-13 01:12:02 +0430

    Client http request implemented and Apache HTTPClient dependencies added.

--- Learning Requirements/Rest/src/main/java/ClientSide/Client.java
+++ Learning Requirements/Rest/src/main/java/ClientSide/Client.java
@@ -1,7 +1,37 @@
 package ClientSide;
 
+
+import Entity.AddObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.nio.charset.StandardCharsets;
+
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        String uri = "http://localhost:4567/add";
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(uri);
+
+        String jSonString = null;
+        /*
+            jSonString is an instance of AddObject converted to JSON as a string
+            for example:
+            jSonString = "{\"number1\":1,\"number2\":3,\"sum\":5}";
+
+        */
+        HttpEntity entity = new ByteArrayEntity(jSonString.getBytes(StandardCharsets.UTF_8));
+        post.setEntity(entity);
+        HttpResponse response = client.execute(post);
+        String result = EntityUtils.toString(response.getEntity());
+        System.out.println(result);
 
     }
 }
