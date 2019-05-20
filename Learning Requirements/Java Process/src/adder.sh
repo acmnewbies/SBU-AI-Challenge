f1fe1226132785dc76e96f3c1c76cac7df8caecd
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2019-05-20 14:52:15 +0430

    adder.sh is corrected.

--- Learning Requirements/Java Process/src/adder.sh
+++ Learning Requirements/Java Process/src/adder.sh
@@ -2,8 +2,8 @@
 cd /home/roozbeh/MyApps/university/Term\ 4/SBU-AI-Challenge/Learning\ Requirements/Java\ Process/src/
 CLASSFILE=TwoNumbersAdder.class
 if [ -f "$CLASSFILE" ]; then
-	java TwoNumbersAdder $NUM1 $NUM2
+	java TwoNumbersAdder
 else
 	javac TwoNumbersAdder.java
-	java TwoNumbersAdder $NUM1 $NUM2
+	java TwoNumbersAdder
 fi
