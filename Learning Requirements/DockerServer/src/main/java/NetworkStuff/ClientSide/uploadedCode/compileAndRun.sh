fe1539451599afea9482c5c4c59b603fa071f582
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2019-05-27 15:46:44 +0430

    "CompileAndRun.sh" had some trouble finding the intended code.

--- Learning Requirements/DockerServer/src/main/java/NetworkStuff/ClientSide/uploadedCode/compileAndRun.sh
+++ Learning Requirements/DockerServer/src/main/java/NetworkStuff/ClientSide/uploadedCode/compileAndRun.sh
@@ -1,2 +1,3 @@
-g++ uploadedCode.cpp -o uploadedCode -Wall -g -O2 -std=c++11 
+cd src/main/java/NetworkStuff/ClientSide/uploadedCode/
+g++ uploadedCode.cpp -o uploadedCode -Wall -g -O2 -std=c++11
 ./uploadedCode
