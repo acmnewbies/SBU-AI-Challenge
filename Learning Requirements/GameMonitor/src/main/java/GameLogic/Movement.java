1b69352683a4e9352f8ab57c78a519ccd473cbaf
amirali monjar <amirali.mnj@gmail.com>
2019-05-28 14:10:39 +0430

    Frame Rates sycronized.

--- Learning Requirements/GameMonitor/src/main/java/GameLogic/Movement.java
+++ Learning Requirements/GameMonitor/src/main/java/GameLogic/Movement.java
@@ -1,13 +1,20 @@
 package GameLogic;
 
+import Monitor.FrameNumber;
+
 public class Movement {
     private Cord src, dist;
-
-    public Movement(Cord src , Cord dist){
+    private long frames;
+    public Movement(Cord src , Cord dist, FrameNumber frames){
+        this.frames = frames.getValue();
         this.src = new Cord(src.getX(),src.getY());
         this.dist = new Cord(dist.getX(),dist.getY());
 
 
+    }
+
+    public long getFrames() {
+        return frames;
     }
 
     public Cord getDist() {
--- Learning Requirements/GameMonitor/src/main/java/Test.java
+++ Learning Requirements/GameMonitor/src/main/java/Test.java
@@ -3,7 +3,7 @@ import Monitor.GameMonitor;
 public class Test  {
 
     public static void main(String[] args)  {
-        GameMonitor gm = new GameMonitor(800 , 400 , 50,true );
+        GameMonitor gm = new GameMonitor(800 , 400 , 10,false );
     }
 
 }
