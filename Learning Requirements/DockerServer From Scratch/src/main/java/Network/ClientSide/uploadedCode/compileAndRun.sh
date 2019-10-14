9a9f9f9acee6be81ee1c890bcfd275a85e58ac70
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2019-10-14 16:55:15 +0330

    Server can now run python clients too.

--- Learning Requirements/DockerServer From Scratch/src/main/java/Network/ClientSide/uploadedCode/compileAndRun.sh
+++ Learning Requirements/DockerServer From Scratch/src/main/java/Network/ClientSide/uploadedCode/compileAndRun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd Network/ClientSide/uploadedCode/
-g++ uploadedCode.cpp -o uploadedCode -Wall -g -O2 -std=c++11
-./uploadedCode
+#g++ uploadedCode.cpp -o uploadedCode -Wall -g -O2 -std=c++11
+#./uploadedCode
+python3 uploadedCode.py
